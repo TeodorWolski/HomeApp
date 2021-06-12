3a800b7afc2885e63443805805152365d6497f65
Teodor Wolski <teodorwolski88@gmail.com>
2021-06-12 11:22:30 +0200

    Application finished

---
+++ home-app/src/assets/data/data.js
@@ -0,0 +1,6 @@
+export const staticData = [
+  { name: 'jabłka', id: 1, description: 'antonówki, zielone, czerwone' },
+  { name: 'kapusta', id: 2, description: 'kapusta czerwona i zielnona ale nie sałata' },
+  { name: 'zegar', id: 3, description: 'wskazówkowy, z antykwariatu' },
+  { name: 'klawiatura', id: 4, description: 'mechaniczna, turbodynamiczna' },
+];
--- home-app/src/views/ShoppingPage.js
+++ home-app/src/views/ShoppingPage.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import styled from 'styled-components';
+import Heading from 'components/atoms/Heading/Heading';
+import { staticData } from 'assets/data/data';
 import UserTemplate from 'templates/UserTemplate';
+import TaskCard from 'components/molecules/TaskCard/TaskCard';
+
+const StyledGrid = styled.div`
+  display: grid;
+  grid-gap: 30px;
+  margin-top: 5%;
+  grid-template-columns: repeat(3, 1fr);
+`;
+
+const StyledHeading = styled(Heading)`
+  margin-top: 0;
+  padding-top: 40px;
+  display: block;
+`;
 
 const ShoppingPage = () => (
   <UserTemplate>
-    <div>This is shopping template</div>
+    <StyledHeading big>Twoje dzisiejsze zadania</StyledHeading>
+    <StyledGrid>
+      {staticData.map(({ name, id, description }) => (
+        <TaskCard name={name} key={id} description={description} />
+      ))}
+    </StyledGrid>
   </UserTemplate>
 );
 
